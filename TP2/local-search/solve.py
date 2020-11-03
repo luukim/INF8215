306b82dffe4d105c559d78aacb4a48f25f5fa50e
Kim Luu <thien-kim.luu@polymtl.ca>
2020-11-03 18:13:00 -0500

    refactored solve.py

--- TP2/local-search/solve.py
+++ TP2/local-search/solve.py
@@ -1,4 +1,7 @@
 # -*- coding: utf-8 -*-
+# Thien-Kim Loisel-Luu 1834378
+# Souhaila Melloul 1835144
+# Mourad Younes 1832387
 from generator_problem import GeneratorProblem
 import random
 
@@ -10,37 +13,13 @@ class Solve:
         self.n_generator = n_generator
         self.n_device = n_device
         self.seed = seed
-
         self.instance = GeneratorProblem.generate_random_instance(self.n_generator, self.n_device, self.seed)
 
-    def solve_naive(self):
-
-        print("Solve with a naive algorithm")
-        print("All the generators are opened, and the devices are associated to the closest one")
-
-        opened_generators = [1 for _ in range(self.n_generator)]
-
-        assigned_generators = [None for _ in range(self.n_device)]
-
-        for i in range(self.n_device):
-            closest_generator = min(range(self.n_generator),
-                                    key=lambda j: self.instance.get_distance(self.instance.device_coordinates[i][0],
-                                                                      self.instance.device_coordinates[i][1],
-                                                                      self.instance.generator_coordinates[j][0],
-                                                                      self.instance.generator_coordinates[j][1])              
-                                    )
-
-            assigned_generators[i] = closest_generator
-
-        self.printSolution(assigned_generators, opened_generators)
-
-    def solve_localSearch(self):
+    def solve(self):
         print("Solve with a local search algorithm")
-        generators = list(range(0, self.n_generator))
         opened_generators = [0 for _ in range(self.n_generator)]
         assigned_generators = [None for _ in range(self.n_device)]
-        costs = {}
-        #solution initiale
+
         for i in range(self.n_device):
             assigned_generators[i] = random.randint(0, self.n_generator-1)
 
@@ -48,12 +27,11 @@ class Solve:
             j = assigned_generators[i]
             opened_generators[j] = 1
 
-        solutions_found = {} #pour stocker toutes les solutions trouvées et retourner à une meilleure précédente si nécessaire
+        solutions_found = {}
         best_solution = assigned_generators, opened_generators
         solutions_found[self.instance.get_solution_cost(best_solution[0], best_solution[1])] = best_solution
-        print("initial solution", self.instance.get_solution_cost(best_solution[0], best_solution[1]))
         banned_generators = []
-        #voisins possibles        
+            
         for i in range(self.n_device*5):
             neighbors = self.getNeighbors(assigned_generators, opened_generators, banned_generators)
             countGenerator = 0
@@ -103,47 +81,8 @@ class Solve:
     def printSolution(self, assigned_generators, opened_generators) :
         self.instance.solution_checker(assigned_generators, opened_generators)
         total_cost = self.instance.get_solution_cost(assigned_generators, opened_generators)
-        self.instance.plot_solution(assigned_generators, opened_generators, "local_search")
+        self.instance.plot_solution(assigned_generators, opened_generators)
 
         print("[ASSIGNED-GENERATOR]", assigned_generators)
         print("[OPENED-GENERATOR]", opened_generators)
         print("[SOLUTION-COST]", total_cost)
-
-    def getDistance(self, i, j) :
-        return self.instance.get_distance(self.instance.device_coordinates[i][0],
-                    self.instance.device_coordinates[i][1],
-                    self.instance.generator_coordinates[j][0],
-                    self.instance.generator_coordinates[j][1])
-
-    def getZone(self, id, type):
-        coordinates = (0,0)
-        if type == 1 :
-            coordinates = self.instance.generator_coordinates[id]
-        elif type == 2 :
-            coordinates = self.instance.device_coordinates[id]
-        if (coordinates[0] >= 0 and 
-            coordinates[0] <=  50 and
-            coordinates[1] >= 50 and
-            coordinates[1] <= 100) :
-            return 1
-        elif (coordinates[0] >= 50 and
-        coordinates[0] <=  100 and
-        coordinates[1] >= 50 and
-        coordinates[1] <= 100) :
-            return 2
-        elif (coordinates[0] >= 0 and 
-        coordinates[0] <=  50 and
-        coordinates[1] >= 0 and
-        coordinates[1] <= 100) :
-            return 3
-        elif (coordinates[0] >= 50 and 
-        coordinates[0] <=  100 and
-        coordinates[1] >= 0 and
-        coordinates[1] <= 50) :
-            return 4
-
-
-
-
-
-
